de8b2b2d49b5f480c4ceb590428e260445c1d860
matansharon <49459070+matansharon@users.noreply.github.com>
2021-02-17 21:40:59 +0200

    Add files via upload

---
+++ maman14/q2/Tester.java
@@ -0,0 +1,14 @@
+
+/**
+ *  class Tester simply call to the appliction GUI and start the running of the program 
+ * 
+ * @author Matan Sharon 
+ * @version 13.5.2020
+ */
+public class Tester
+{
+    public static void main(String args[])
+    {
+       Appliction p=new Appliction();
+    }
+}
