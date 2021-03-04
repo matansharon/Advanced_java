9024be63a6054e119d825f8e20e694f366f4b913
matansharon <49459070+matansharon@users.noreply.github.com>
2021-03-04 22:08:00 +0200

    Add files via upload

---
+++ sportManeger/src/Tester.java
@@ -0,0 +1,47 @@
+public class Tester {
+    public static void main(String []args)
+    {
+        chooseTeam chooseTeam = new chooseTeam("nba");
+        /*
+        NBA nba=new NBA();
+        Stack<Team> list=new Stack<>();
+        Random random = new Random();
+
+        while(list.size()<30)
+        {
+            Team team = nba.getMainList().get(random.nextInt(30));
+
+            if(!(list.contains(team))){list.push(team);}
+        }
+
+
+
+        while(!list.isEmpty()) {
+            Team t1 = list.pop();
+            Team t2 = list.pop();
+            t1.scadual.add(t2);
+            t2.scadual.add(t2);
+
+            System.out.printf("%s vs %s\n",t1.getName(),t2.getName());
+
+
+        }
+
+
+        int i=0;
+        while(i!=-1)
+        {
+            int j=Integer.parseInt(JOptionPane.showInputDialog(null,"choose team"));
+            Team t=nba.getMainList().get(j);
+            System.out.println(t.getName()+" "+t.scadual.get(0));
+        }
+
+         */
+
+
+
+
+
+
+    }
+}
