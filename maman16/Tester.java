fdcf75c9e2e5e897aeb5db0b7f7dd13de899a521
matansharon <49459070+matansharon@users.noreply.github.com>
2021-03-07 22:29:47 +0200

    Add files via upload

---
+++ maman16/Tester.java
@@ -0,0 +1,16 @@
+import java.net.*; 
+import java.io.*;
+/**
+ * Write a description of class Tester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tester
+{
+    public static void main(String[]args) throws IOException
+    {
+        
+        GUI g=new GUI();
+    }
+}
