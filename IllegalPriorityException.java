3400c677fa81d23233643b63512d9b724d11e22b
matansharon <49459070+matansharon@users.noreply.github.com>
2021-04-09 23:00:33 +0300

    Add files via upload

---
+++ IllegalPriorityException.java
@@ -0,0 +1,18 @@
+/*
+ * IllegalPriorityException.java
+ *
+ * Represents an illegal priority value
+ *
+ * 
+ */
+
+/**
+ *
+ * @author Shay Tavor, shay.tavor@gmail.com
+ */
+public class IllegalPriorityException  extends Exception {
+    public IllegalPriorityException(String msg) {
+        super(msg);
+    }
+    
+}
